cff047fa8838e0e6ff595a13ebfa0318884a6fc6
ThaMessia <simple35@virgilio.it>
2021-10-08 21:55:19 +0200

    Initial commit

---
+++ src/main/java/studio/thamessia/ShadowLux.java
@@ -0,0 +1,17 @@
+package studio.thamessia;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ShadowLux extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
