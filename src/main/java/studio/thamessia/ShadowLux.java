e369096c225be16bf26f1036babdab87b783691b
ThaMessia <simple35@virgilio.it>
2021-10-09 15:13:50 +0200

    Added customizable config.yml

--- src/main/java/studio/thamessia/ShadowLux.java
+++ src/main/java/studio/thamessia/ShadowLux.java
@@ -1,14 +1,21 @@
 package studio.thamessia;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ShadowLux extends JavaPlugin {
+    private FileConfiguration configuration = this.getConfig();
 
     @Override
     public void onEnable() {
+        configuration = this.getConfig();
+
         System.out.println("WARNING: THIS IS ONLY FOR DEVELOPERS' USE.");
         System.out.println("[ShadowLux] Plugin enabled!");
+
+        if (!configuration.getBoolean("ShadowLux")) return;
+
         Bukkit.getPluginManager().registerEvents(new PacketSniffer(), this);
     }
 
